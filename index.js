0ed9c26947ea73bce7f9ddf672a7fdc74b9656e9
lolergb <lole@bynewroman.com>
2025-12-19 22:00:09 +0100

    fixed(manifest) fixed sdk

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import OBR from "https://unpkg.com/@owlbear-rodeo/sdk@1/dist/index.esm.js";
+import OBR from "https://cdn.jsdelivr.net/npm/@owlbear-rodeo/sdk@3.1.0/dist/index.esm.js";
 
 // Configuración de páginas de Notion
 // Agrega aquí tus páginas públicas de Notion
